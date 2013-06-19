206e512d5bbf76ceeafa3041ab4e615e956e8ce7
nrodday <nils.rodday@fh-muenster.de>
2013-06-19 16:16:01 +0200

    PhoneGap Initialization and Hybrid Project

---
+++ HybridApp/src/sernet/verinice/hybrid/Hybrid.java
@@ -0,0 +1,15 @@
+package sernet.verinice.hybrid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Hybrid extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
