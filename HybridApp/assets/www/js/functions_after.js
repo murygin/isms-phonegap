9407525e79cf3ffcf03f7e105b6baace127775fb
nrodday <nils.rodday@fh-muenster.de>
2013-06-24 12:31:13 +0200

    connection and login - panel bar

--- HybridApp/assets/www/js/functions_after.js
+++ HybridApp/assets/www/js/functions_after.js
@@ -1,27 +1,29 @@
+$( document ).bind( 'mobileinit', function(){
+  $.mobile.loader.prototype.options.text = "loading";
+  $.mobile.loader.prototype.options.textVisible = false;
+  $.mobile.loader.prototype.options.theme = "a";
+  $.mobile.loader.prototype.options.html = "";
+});
+
 $(document).ready(function() {
-			var ip_address = $("#ip_address").val();
-			var port = $("#port").val();
+			
+			var ip_address = "192.168.2.104";
+			var port = "8080";
 			var user = "";
 			var password = "";
 			
-			$("#ip_address").change(function() {
-				ip_address = $("#ip_address").val();
-			})
-			$("#port").change(function() {
-				port = $("#port").val();
-			})
-		
-	    	$("#button").click(function() {
-	    		alert("Click");
+			
+
+	    	$("#connection_button").click(function() {
+
 	    		$.ajax({ 
 	    			 type: "GET",   
 	    	         url: "http://" + ip_address + ":" + port + "/veriniceserver/rest/json/metallica/todo",
 	    	         username: user,
 	    	         password: password,
-	    	         async: false,
+	    	         async: false, 
 	    	         success : function(data)
-	    	         {	
-	    	        	 
+	    	         {		 
 	    	        	 $.each(data, function(i,item){
 	    	        	 	//alert(item.title);	    	        	 	
 	    	        	 	$('#bsi_controls').append("<div id='line"+i+"' style='display:table-row'></div>");
@@ -30,37 +32,78 @@ $(document).ready(function() {
 	    	        	 	$('#line'+i).append("<div class='third_column'>"+item.umsetzung+"</div>");
 	    	        	 	$('#line'+i).append("<div class='fourth_column'>"+item.umsetzungBis+"</div>");
 	    	        	 });
+	    	         },
+	    	         error: function(jqXHR, exception) {
+	    	        	 if (jqXHR.status === 0) {
+	    	                 alert('Not connect.\n Verify Network.');
+	    	             } else if (jqXHR.status == 404) {
+	    	                 alert('Requested page not found. [404]');
+	    	             } else if (jqXHR.status == 500) {
+	    	                 alert('Internal Server Error [500].');
+	    	             } else if (exception === 'parsererror') {
+	    	                 alert('Requested JSON parse failed.');
+	    	             } else if (exception === 'timeout') {
+	    	                 alert('Time out error.');
+	    	             } else if (exception === 'abort') {
+	    	                 alert('Ajax request aborted.');
+	    	             } else if (jqXHR.responseText.indexOf("This request requires HTTP authentication") != -1){
+	    	                 alert("Der Server benötigt eine Authentifizierung! - Ihre Zugangsdaten sind falsch!");
+	    	             } else {
+	    	                 alert('Uncaught Error.\n' + jqXHR.responseText);
+	    	             }
+
 	    	         }
-	    		});
+	    		});	
+	    		
 	    	})
+	    	
+	    	
 	    	$(".first_column").click(function() {
 				alert("click");
 			})
+	    	
+	    	//Error Box
+	    	$("#connection_error_button").click(function() {
+	    		$("#signIn").popup("close");
+	    	})
+	    	
+	    	$("#test").click(function() {	    		
+	    		 $("#connection_error").popup("open");	
+	    	})
 			
+			//Login button clicked
 			$("#signIn_save").click(function() {
-				var user = $('#signIn_user').val();
-				var password = $('#signIn_password').val();
-				$("#signIn").hide();
+				user = $('#signIn_user').val();
+				password = $('#signIn_password').val();
+				$("#signIn").popup("close");
 				alert(user);
 				alert(password);
 			})
 
-			$("#signIn_link").click(function() {
-				$("#signIn").show();
-			})
 			$("#signIn_cancel").click(function() {
-				$("#signIn").hide();
+				$("#signIn").popup("close");
 			})
 			
+			//Connection button clicked
+			$("#connection_save").click(function() {
+				var ip_address = $('#connection_ip_address').val();
+				var port = $('#connection_port').val();
+				$("#connection_box").popup("close");
+				alert(ip_address);
+				alert(port);
+			})
+
+			$("#connection_cancel").click(function() {
+				$("#connection_box").popup("close");
+			})
 			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+			//Panel height setter
+			$( "#panel_right" ).on({
+			    popupbeforeposition: function() {
+			        var h = $( window ).height();
+
+			        $( "#panel_right" ).css( "height", h );
+			    }
+			});
+
 })
